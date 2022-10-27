d4335bbbf245a60d23041296e85ada6c77844d74
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-10-27 20:55:59 +0530

    Added:Welcome

---
+++ src/com/bridgelabz/addressbook/Contact.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.addressbook;
+
+public class Contact {
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String EmailId;
+	private String city;
+	private String state;
+	private String zip;
+}
